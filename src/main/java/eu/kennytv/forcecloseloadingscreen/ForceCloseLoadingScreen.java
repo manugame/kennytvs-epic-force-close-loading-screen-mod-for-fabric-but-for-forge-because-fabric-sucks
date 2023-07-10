30219f724702abddb1e350cbcd2950c2effa72ca
manugame <manugameur.pro@gmail.com>
2023-07-10 22:27:25 +0200

    port to forge (i hate forge too <3)

---
+++ src/main/java/eu/kennytv/forcecloseloadingscreen/ForceCloseLoadingScreen.java
@@ -0,0 +1,7 @@
+package eu.kennytv.forcecloseloadingscreen;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("forcecloseloadingscreen")
+public class ForceCloseLoadingScreen {
+}
--- src/main/java/eu/kennytv/forcecloseloadingscreen/mixin/LoadingOverlayMixin.java
+++ src/main/java/eu/kennytv/forcecloseloadingscreen/mixin/LoadingOverlayMixin.java
@@ -22,8 +22,8 @@
  */
 package eu.kennytv.forcecloseloadingscreen.mixin;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.LoadingOverlay;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
@@ -45,7 +45,7 @@ public abstract class LoadingOverlayMixin {
     private long fadeOutStart;
 
     @Inject(at = @At("TAIL"), method = "render")
-    public void setScreen(final GuiGraphics guiGraphics, final int i, final int j, final float f, final CallbackInfo ci) {
+    public void setScreen(PoseStack p_96178_, int p_96179_, int p_96180_, float p_96181_, CallbackInfo ci) {
         if (this.fadeOutStart != -1) {
             this.minecraft.setOverlay(null);
         }
